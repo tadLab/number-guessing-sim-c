e2124539b0d088c505f2901d7f041e5220118494
tadLab <161091442+tadLab@users.noreply.github.com>
2024-04-14 23:15:24 +0200

    simulator 1.0

--- simulator.c
+++ simulator.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <ctype.h> 
+
+int difficulty(char level[7]) {
+
+    for (int position = 0; level[position] != '\0'; position++) {
+        level[position] = tolower(level[position]);
+    }
+
+    if (strcmp(level, "easy") == 0) {
+        return 0; 
+    } else if (strcmp(level, "medium") == 0) {
+        return 1; 
+    } else if (strcmp(level, "hard") == 0) {
+        return 2; 
+    } else {
+        return -1; 
+    }
+}
 
 void guide() {
     printf("              *WELCOME*            \n");
     printf("*THIS IS A GUESSING NUMBER SIMULATOR*\n");
-    printf("      *GUESS A NUMBER FROM 1-10*     \n");
-    printf("          *YOU HAVE 5 TRIES*         \n");
+    printf("       *CHOOSE YOUR DIFFICULTY*     \n");
+    printf("          *EASY MEDIUM HARD*       \n");
 }
 
 int main() {
@@ -14,22 +33,44 @@ int main() {
     int randomNumber;
     char answerContinue;
     int tries; 
-
-    srand(time(NULL));
+    char level[7];
+    int range;
 
     guide();
 
+    srand(time(NULL));
+
     do {
-        tries = 5; 
+
+        printf("LEVEL: ");
+        scanf("%6s", level); 
+
+        int diff = difficulty(level); 
+
+        if (diff == -1) {
+            printf("Invalid difficulty level. Exiting the game.\n");
+            return 0; 
+        }
+
+        tries = 5;
+
+        if(diff == 0){
+            range = 10;
+        } else if(diff == 1){
+            range = 20;
+        } else if(diff == 2){
+            range = 30;
+        }
+
         while (tries > 0) {
-            randomNumber = 1 + rand() % 10; 
+            randomNumber = 1 + rand() % range; 
             usersNumber = 0; 
             
             while (randomNumber != usersNumber && tries > 0) {
-                printf("TYPE YOUR GUESS (1-10): ");
+                printf("TYPE YOUR GUESS (1-%d): ", range);
 
-                while (scanf("%d", &usersNumber) != 1 || usersNumber < 1 || usersNumber > 10) {
-                    printf("Invalid input. Please enter a number from 1 to 10: ");
+                while (scanf("%d", &usersNumber) != 1 || usersNumber < 1 || usersNumber > range) {
+                    printf("Invalid input. Please enter a number from 1 to %d: ", range);
                     while (getchar() != '\n'); 
                 }
 
